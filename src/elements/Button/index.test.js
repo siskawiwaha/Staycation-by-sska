a40c92c3a78cb75b83d9c34ca049ba59577f55c4
sskawlndr12 <hi.siskawiwaha@gmail.com>
2021-09-12 09:14:11 +0700

    Staycation

--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -1,3 +1,8 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import Button from './index'
+import React from "react";
+import { render } from "@testing-library/react";
+import Button from "./index";
+
+test("Should not allowed click button if isDisable present", () => {
+    const { container } = render(<Button isDisabled></Button>);
+    expect(contianer.querySelector("span-disabled")).toBeInTheDocument();
+});
