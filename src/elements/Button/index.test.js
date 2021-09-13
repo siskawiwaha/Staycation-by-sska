16b5161b1e3336f79996d3455a19df9a577e8697
sskawlndr12 <hi.siskawiwaha@gmail.com>
2021-09-13 09:43:50 +0700

    Staycation by sska

--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -4,5 +4,11 @@ import Button from "./index";
 
 test("Should not allowed click button if isDisable present", () => {
     const { container } = render(<Button isDisabled></Button>);
-    expect(contianer.querySelector("span-disabled")).toBeInTheDocument();
+    expect(container.querySelector("span-disabled")).toBeInTheDocument();
+});
+
+test("Should render loading/spinner", () => {
+    const { container, getByText } = render(<Button isLoading></Button>);
+
+    expect(getByText(/loading/)i).toBeInTheDocument();
 });
---
+++ src/parts/iconText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Button from 'elements/Button';
+
+export default function iconText() {
+    return (
+        <Button className="brand-text-icon" href=" " type="link">
+            Stay<span className="$gray-900"></span>cation.
+        </Button>
+            
+    )
+}
